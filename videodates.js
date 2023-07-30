4134b94390b718cb44e1a874f5fb87c259ccf621
At3k0 <adiramerian@gmail.com>
2023-07-29 17:11:03 -0700

    big update
    added a few more videos to play on dates, fixed the second column looking strange, moved the videos to a folder

--- videodates.js
+++ videodates.js
@@ -1,34 +1,60 @@
 function isJuly25th() {
-        var today = new Date();
-        return today.getMonth() === 6 && today.getDate() === 25;
-    }
+    var today = new Date();
+    return today.getMonth() === 6 && today.getDate() === 25;
+}
 
-    function isAugest11th() {
-        var today = new Date();
-        return today.getMonth() === 7 && today.getDate() === 11;
-    }
-    function isOctober1st() {
-        var today = new Date();
-        return today.getMonth() === 9 && today.getDate() === 1;
-    }
+function isAugest11th() {
+    var today = new Date();
+    return today.getMonth() === 7 && today.getDate() === 11;
+}
 
-    function toggleContent() {
-        var v = document.getElementById("songvideo");
-        var t = document.getElementById("songtext");
+function isSeptember22nd() {
+    var today = new Date();
+    return today.getMonth() === 8 && today.getDate() === 22;
+}
 
-        if (isJuly25th()) {
-            v.src = "birthday.mp4";
-            t.textContent = "july 25th baby";
-        } else if (isAugest11th()) {
-            v.src = "dogdays.mp4";
-            t.textContent = "school time";
-        } else if (isOctober1st()){
-        	v.src = "spooky.mp4";
-        	t.textContent = "spooky begins";
-        } else {
-            v.style.display = "none";
-            t.style.display = "none";
-        }
+function isOctober1st() {
+    var today = new Date();
+    return today.getMonth() === 9 && today.getDate() === 1;
+}
+
+function isDecember1st() {
+    var today = new Date();
+    return today.getMonth() === 11 && today.getDate() === 1;
+}
+
+function isDecember2nd() {
+    var today = new Date();
+    return today.getMonth() === 11 && today.getDate() === 2;
+}
+
+function toggleContent() {
+    var v = document.getElementById("songvideo");
+    var t = document.getElementById("songtext");
+
+    if (isJuly25th()) {
+        v.src = "videodates/birthday.mp4";
+        t.textContent = "july 25th baby";
+    } else if (isAugest11th()) {
+        v.src = "videodates/dogdays.mp4";
+        t.textContent = "summer's gone";
+    } else if (isSeptember22nd()) {
+        v.src = "videodates/schoolsin.mp4"
+        t.textContent = "time for school"
+    } else if (isOctober1st()){
+    	v.src = "videodates/spooky.mp4";
+    	t.textContent = "spooky begins";
+    } else if (isDecember1st()){
+        v.src = "videodates/beginchristmas.mp4";
+        t.textContent = "december day 1 lesson - dont die";
+    } else if (isDecember2nd()){
+        v.src = "videodates/wonderfultime.mp4";
+        t.textContent = "december day 2 lesson - look both ways before crossing the road ";
+    /*ill add more christmas ones later, probably in december because obviously*/
+    } else {
+        v.style.display = "none";
+        t.style.display = "none";
     }
+}
 
-    window.onload = toggleContent;
+window.onload = toggleContent;
