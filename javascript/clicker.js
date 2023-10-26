5548d720ec5f6ec83df0bead282374b28031a839
Alec Diramerian <adiramerian@gmail.com>
2023-10-25 20:57:10 -0700

    new quotes + alec clicker v0.2
    - 5 new quotes
    - alec clicker now autosaves, has a new upgrade tier, and has skins

--- javascript/clicker.js
+++ javascript/clicker.js
@@ -1,16 +1,89 @@
 let nume = 1;
-var clickSFX = new Audio('audio/mcclick.mp3');
-function bclick() {
-	clickSFX.cloneNode().play();
-	nume++;
-	document.getElementById('num').innerText = nume;
-}
+let alectype = 0;
+let skin = 0;
 
-document.getElementById('alec').onmousedown = function() { 
-	document.getElementById('alec').src = 'images/alecfaceupmush.png';
+const clickSFX = new Audio('audio/mcclick.mp3');
+const alec = document.getElementById('alec');
 
-}
+if (localStorage.getItem('nume')) nume = parseInt(localStorage.getItem('nume'));
+if (localStorage.getItem('alectype')) alectype = parseInt(localStorage.getItem('alectype'));
 
-document.getElementById('alec').onmouseup = function() {
-	document.getElementById('alec').src = 'images/alecfaceup.png';
-}
+const saveProgress = () => {
+    localStorage.setItem('nume', nume);
+    localStorage.setItem('alectype', alectype);
+};
+
+setInterval(saveProgress, 60000);
+
+const updateDisplay = () => {
+    document.getElementById('num').innerText = nume;
+    if (skin === 1) {
+		alec.src = alectype === 0 ? 'images/abby.png' : 'images/abby2.png';
+	} else if (skin === 2) {
+		alec.src = alectype === 0 ? 'images/nate.png' : 'images/nate2.png'
+	} else {
+		alec.src = alectype === 0 ? 'images/alecfaceup.png' : 'images/babyalec.png';
+	}
+};
+
+updateDisplay();
+
+document.getElementById('reset').addEventListener('click', () => {
+    const yn = prompt("Do you want to reset? THERE IS NO UNDOING THIS!", "").toLowerCase();
+    if (["yes", "si", "yeah", "sure"].includes(yn)) {
+        nume = 1;
+        alectype = 0;
+        updateDisplay();
+        saveProgress();
+    }
+});
+
+document.getElementById('skin').addEventListener('click', () => {
+    const skinchange = prompt("Which skin do you want to use? (Alec, Abby, or Nate)", "").toLowerCase();
+    if (skinchange === "alec") {
+        skin = 0;
+        alec.src = alectype === 0 ? 'images/alecfaceup.png' : 'images/babyalec.png';
+    } else if (skinchange === "abby") {
+    	skin = 1;
+        alec.src = alectype === 0 ? 'images/abby.png' : 'images/abby2.png';
+    } else if (skinchange === "nate") {
+    	skin = 2;
+        alec.src = alectype === 0 ? 'images/nate.png' : 'images/nate2.png';
+    }
+});
+
+const bclick = () => {
+    clickSFX.cloneNode().play();
+    nume += alectype === 0 ? 1 : 5;
+    document.getElementById('num').innerText = nume;
+    document.getElementById('newalec').style.display = nume >= 50 && alectype === 0 ? "block" : "none";
+    saveProgress();
+};
+
+const nbclick = () => {
+    alec.src = 'images/babyalec.png';
+    alectype++;
+    document.getElementById('newalec').style.display = "none";
+    updateDisplay();
+    saveProgress();
+};
+
+alec.addEventListener('mousedown', () => {
+	if (skin === 1) {
+		alec.src = alectype === 0 ? 'images/abbymush.png' : 'images/abby2mush.png';
+	} else if (skin === 2) {
+		alec.src = alectype === 0 ? 'images/natemush.png' : 'images/nate2mush.png';
+	} else {
+	    alec.src = alectype === 0 ? 'images/alecfaceupmush.png' : 'images/babyalecmush.png';
+	}
+});
+
+alec.addEventListener('mouseup', () => {
+	if (skin === 1) {
+		alec.src = alectype === 0 ? 'images/abby.png' : 'images/abby2.png';
+	} else if (skin === 2) {
+		alec.src = alectype === 0 ? 'images/nate.png' : 'images/nate2.png'
+	} else {
+		alec.src = alectype === 0 ? 'images/alecfaceup.png' : 'images/babyalec.png';
+	}
+});
--- javascript/quotepages.js
+++ javascript/quotepages.js
@@ -5,12 +5,13 @@ const pageTitlesQ = [
     "HS Quotes",
     "HS Quotes",
     "HS Quotes",
+    "HS Quotes",
     "MS Quotes",
     "MS Quotes"
 ];
 
 function showPageQ(pageNumberQ) {
-    for (let i = 1; i <= 7; i++) {
+    for (let i = 1; i <= 8; i++) {
         document.querySelector(`.qpage.qpage-${i}`).classList.remove("active");
     }
 
@@ -21,11 +22,11 @@ function showPageQ(pageNumberQ) {
 }
 
 function goForwardQ() {
-    showPageQ(currentPageQ < 7 ? currentPageQ + 1 : 1);
+    showPageQ(currentPageQ < 8 ? currentPageQ + 1 : 1);
 }
 
 function goBackQ() {
-    showPageQ(currentPageQ > 1 ? currentPageQ - 1 : 7);
+    showPageQ(currentPageQ > 1 ? currentPageQ - 1 : 8);
 }
 
 showPageQ(currentPageQ);
