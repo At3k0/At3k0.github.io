42390e4bd8ee4e28d6be76ee46f31a63352c913c
Alec Diramerian <adiramerian@gmail.com>
2024-05-16 22:04:48 -0700

    b

--- javascript/quotepages.js
+++ javascript/quotepages.js
@@ -11,11 +11,12 @@ const pageTitlesQ = [
 	"HS Quotes",
 	"HS Quotes",
 	"MS Quotes",
+	"MS Quotes",
 	"MS Quotes"
 ];
 
 function showPageQ(pageNumberQ) {
-	for (let i = 1; i <= 12; i++) {
+	for (let i = 1; i <= 13; i++) {
 		document.querySelector(`.qpage.qpage-${i}`).classList.remove("active");
 	}
 
@@ -26,11 +27,11 @@ function showPageQ(pageNumberQ) {
 }
 
 function goForwardQ() {
-	showPageQ(currentPageQ < 12 ? currentPageQ + 1 : 1);
+	showPageQ(currentPageQ < 13 ? currentPageQ + 1 : 1);
 }
 
 function goBackQ() {
-	showPageQ(currentPageQ > 1 ? currentPageQ - 1 : 12);
+	showPageQ(currentPageQ > 1 ? currentPageQ - 1 : 13);
 }
 
 showPageQ(currentPageQ);
