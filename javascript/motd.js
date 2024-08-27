8bbc374f1d23f45f4c1ecb4f9688e239644d6096
Alec Diramerian <adiramerian@gmail.com>
2024-08-27 13:28:39 -0700

    Update motd.js

--- javascript/motd.js
+++ javascript/motd.js
@@ -1,32 +1,43 @@
-function updateDateMessage() {
-	const date = new Date();
-	const month = date.getMonth() + 1;
-	const day = date.getDate();
-	let message = ``;
+const date = new Date();
+const month = date.getMonth() + 1;
+const day = date.getDate();
+let message = ``;
 
-	if (month === 8 && day === 27) {
-		message = `"She Bridges on my Hall till I Fame" -Someone, probably`;
-	} else if (month === 8 && day === 28) {
-		message = `make sure to SMASH that like button AND subscribe and hit that bell`;
-	} else if (month === 8 && day === 29) {
-		message = `what if instead of bridges academy its 𝓯𝓻𝓮𝓪𝓴𝔂 academy and instead of educating the exceptional they educate the 𝓯𝓻𝓮𝓪𝓴𝔂`;
-	} else if (month === 8 && day === 30) {
-		message = `SING US A SONG, YOU'RE THE FLANNELMAN🔥🔥🗣️🔥🔥🗣️🔥🗣️🗣️🔥🗣️`;
-	} else if (month === 8 && day === 31) {
-		message = `the birds watch. do not trust them.`;
-	} else if (month === 9 && day === 1) {
-		message = `500 beers 🥰`;
-	} else if (month === 9 && day === 2) {
-		message = `happy britdhay keuanu reeves`;
-	} else if (month === 9 && day === 3) {
-		message = `pancakes are pretty good`;
-	} else if (month === 9 && day === 4) {
-		message = `chair 🪑`;
-	} else if (month === 9 && day === 5) {
-		message = `bridgeshof.com does not pay taxes. nice try feds.`;
-	} else {
-		getElementById("motd").remove()
-	}
-	document.getElementById(`motdmessage`).textContent = message;
+if (month === 8 && day === 27) {
+	message = `"She Bridges on my Hall till I Fame" -Someone, probably`;
+} else if (month === 8 && day === 28) {
+	message = `make sure to SMASH that like button AND subscribe and hit that bell`;
+} else if (month === 8 && day === 29) {
+	message = `what if instead of bridges academy its 𝓯𝓻𝓮𝓪𝓴𝔂 academy and instead of educating the exceptional they educate the 𝓯𝓻𝓮𝓪𝓴𝔂`;
+} else if (month === 8 && day === 30) {
+	message = `SING US A SONG, YOU'RE THE FLANNELMAN🔥🔥🗣️🔥🔥🗣️🔥🗣️🗣️🔥🗣️`;
+} else if (month === 8 && day === 31) {
+	message = `the birds watch. do not trust them.`;
+} else if (month === 9 && day === 1) {
+	message = `500 beers 🥰`;
+} else if (month === 9 && day === 2) {
+	message = `happy britdhay keuanu reeves`;
+} else if (month === 9 && day === 3) {
+	message = `pancakes are pretty good`;
+} else if (month === 9 && day === 4) {
+	message = `chair 🪑`;
+} else if (month === 9 && day === 5) {
+	message = `bridgeshof.com does not pay taxes. nice try feds.`;
+} else if (month === 9 && day === 6) {
+	message = `newsies more like oldsies`;
+} else if (month === 9 && day === 7) {
+	message = `today is national september 7th day`;
+} else if (month === 9 && day === 8) {
+	message = `My palms are sweaty, knees weak, arms are heavy, there's vomit on my flannel already, I cried with Freddie.`;
+} else if (month === 9 && day === 9) {
+	message = `did you know that suicide is so harmful that it could actually kill you?`;
+} else if (month === 9 && day === 10) {
+	message = `!!! <<THIS MESSAGE HAS BEEN EATEN BY A BEAR>> !!!`;
+} else if (month === 9 && day === 11) {
+	message = `im not gonna get cancelled so no motd today`;
+} else if (month === 9 && day === 12) {
+	message = `today is a good day`;
+} else {
+	getElementById("motd").remove()
 }
-updateDateMessage();
+document.getElementById(`motdmessage`).textContent = message;
