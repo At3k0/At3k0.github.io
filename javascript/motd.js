b59d5395021d746ffceb2ea0e0d49fa458b9db06
Alec Diramerian <adiramerian@gmail.com>
2024-09-02 10:42:42 -0700

    added more motd

--- javascript/motd.js
+++ javascript/motd.js
@@ -10,7 +10,7 @@ if (month === 8 && day === 27) {
 } else if (month === 8 && day === 29) {
 	message = `what if instead of bridges academy its 𝓯𝓻𝓮𝓪𝓴𝔂 academy and instead of educating the exceptional they educate the 𝓯𝓻𝓮𝓪𝓴𝔂`;
 } else if (month === 8 && day === 30) {
-	message = `SING US A SONG, YOU'RE THE FLANNELMAN🔥🔥🗣️🔥🔥🗣️🔥🗣️🗣️🔥🗣️`;
+	message = `"Sing us a song, you're the flannelman" -Billy Joel in another universe, probably`;
 } else if (month === 8 && day === 31) {
 	message = `the birds watch. do not trust them.`;
 } else if (month === 9 && day === 1) {
@@ -28,15 +28,15 @@ if (month === 8 && day === 27) {
 } else if (month === 9 && day === 7) {
 	message = `today is national september 7th day`;
 } else if (month === 9 && day === 8) {
-	message = `My palms are sweaty, knees weak, arms are heavy, there's vomit on my flannel already, I cried with Freddie.`;
+	message = `"My palms are sweaty, knees weak, arms are heavy, there's vomit on my flannel already, I cried with Freddie" -Eminem in another universe, probably`;
 } else if (month === 9 && day === 9) {
 	message = `did you know that suicide is so harmful that it could actually kill you?`;
 } else if (month === 9 && day === 10) {
-	message = `!!! <<THIS MESSAGE HAS BEEN EATEN BY A BEAR>> !!!`;
+	message = `today is a good day`;
 } else if (month === 9 && day === 11) {
 	message = `im not gonna get cancelled so no motd today`;
 } else if (month === 9 && day === 12) {
-	message = `today is a good day`;
+	message = `!!! <<THIS MESSAGE HAS BEEN EATEN BY A BEAR>> !!!`;
 } else if (month === 9 && day === 13) {
 	message = `this bridges hof is enhanced with bridges' fastplay`;
 } else if (month === 9 && day === 14) {
@@ -50,7 +50,41 @@ if (month === 8 && day === 27) {
 } else if (month === 9 && day === 18) {
 	message = `bridgeshof.com - hall of faming since 2023`;
 } else if (month === 9 && day === 19) {
-	message = `nah`;
+	message = `if i had to choose man or bear id choose bear because the man has a 5% chance to be a child diddler`;
+} else if (month === 9 && day === 20) {
+	message = `143 drops today!!!!!`;
+} else if (month === 9 && day === 21) {
+	message = `have i ever mentioned that i like flannels?`;
+} else if (month === 9 && day === 22) {
+	message = `"'Cause I'm back, Yes, I'm back, Well, I'm back, Yes, I'm back, Well, I'm back, back, Well, I'm red and black, Yes, I'm red and black" -AC/DC in another universe, probably`;
+} else if (month === 9 && day === 23) {
+	message = `sometimes you gotta hit em with the "I see Mars, I see Venus, I even see your micropenis" and boom, you're set`;
+} else if (month === 9 && day === 24) {
+	message = `greg would not like the titanic because theres an iceberg and he's deathly afraid of those. be careful out there!`;
+} else if (month === 9 && day === 25) {
+	message = `ok so deadpool wears red and black, i wear red and black. deadpool is out of pocket and (most likely) autistic, i'm out of pocket and (most definitely) autistic. deadpool is ugly but has rizz, i'm ugly but i have rizz. deadpool is awesome, i'm awesome. therefore, i am deadpool, stay with me now...`;
+} else if (month === 9 && day === 26) {
+	message = `"You're red then you're black, you're hall then you're fame, you're black then you're red, you're fame then you're hall. You're Wong when it's WAHHH, it's black and it's red, we fight we break up, we diss we make up." -Katy Perry in another universe, probably`;
+} else if (month === 9 && day === 27) {
+	message = `okay, hear me out... https://i.pinimg.com/474x/fe/93/44/fe93448ecdcb26f9e4dfecb8c0df3b23.jpg`;
+} else if (month === 9 && day === 28) {
+	message = `does alicia keys have keys? if so, is lady gaga a baby? is eminem a candy? is p!nk pink? how green is green day? did the killers kill anyone? did one direction only tour north? how fast is taylor swift? why doesnt twenty one pilots have twenty one band members? we may never know`;
+} else if (month === 9 && day === 29) {
+	message = `ron weasley is just young ed sheeren`;
+} else if (month === 9 && day === 30) {
+	message = `also taylor swift is just young gwyneth paltrow`;
+} else if (month === 10 && day === 1) {
+	message = `BOO! ...i didnt get you, did i?`;
+} else if (month === 10 && day === 2) {
+	message = `"'Cause this is flannel, flannel night, and no one's gonna save you from the hall about to fame, you know it's flannel, flannel night, you're fighting for your life inside a channel, flannel tonight" -Michael Jackson in another universe, probably`;
+} else if (month === 10 && day === 3) {
+	message = `imagine if neil diamond sung a spookier version of Sweet Caroline called Sweet Coraline for the critically acclaimed movie Coraline`;
+} else if (month === 10 && day === 4) {
+	message = `"Wake up in the mornin' feelin' like P. Diddy, grab my flan-" oh wait... i'll try again tomorrow`;
+} else if (month === 10 && day === 5) {
+	message = `"Walk for me, flannel, I'll be Diddy, you'll be Naomi, woah-oh, le-" OH RIGHT... yeah i'll stop now`;
+} else if (month === 10 && day === 6) {
+	message = `for my birthday i want a flannel cake`;
 } else {
 	getElementById("motd").remove()
 }
